10fb739776bf2f98c632225b6c482f5812bee4a5
Johannes Zirngibl <zirngibl@net.in.tum.de>
2022-07-12 12:37:48 +0200

    unify usage of constants

--- src/probe_modules/module_ipv6_tcp_synscan.c
+++ src/probe_modules/module_ipv6_tcp_synscan.c
@@ -26,6 +26,9 @@
 #include "probe_modules.h"
 #include "packet.h"
 
+#define ZMAP_TCP_SYNSCAN_TCP_HEADER_LEN 20
+#define ZMAP_TCP_SYNSCAN_PACKET_LEN 74
+
 probe_module_t module_ipv6_tcp_synscan;
 static uint32_t num_ports;
 
@@ -49,7 +52,7 @@ int ipv6_synscan_init_perthread(void* buf, macaddr_t *src,
 	struct ether_header *eth_header = (struct ether_header *) buf;
 	make_eth_header_ethertype(eth_header, src, gw, ETHERTYPE_IPV6);
 	struct ip6_hdr *ip6_header = (struct ip6_hdr*)(&eth_header[1]);
-	uint16_t payload_len = sizeof(struct tcphdr);
+	uint16_t payload_len = ZMAP_TCP_SYNSCAN_TCP_HEADER_LEN;
 	make_ip6_header(ip6_header, IPPROTO_TCP, payload_len);
 	struct tcphdr *tcp_header = (struct tcphdr*)(&ip6_header[1]);
 	make_tcp_header(tcp_header, dst_port, TH_SYN);
@@ -72,10 +75,10 @@ int ipv6_synscan_make_packet(void *buf, size_t *buf_len, UNUSED ipaddr_n_t src_i
 				probe_num, validation));
 	tcp_header->th_seq = tcp_seq;
 	tcp_header->th_sum = 0;
-	tcp_header->th_sum = tcp6_checksum(sizeof(struct tcphdr),
+	tcp_header->th_sum = tcp6_checksum(ZMAP_TCP_SYNSCAN_TCP_HEADER_LEN,
 			&ip6_header->ip6_src, &ip6_header->ip6_dst, tcp_header);
 
-	*buf_len = 78;
+	*buf_len = ZMAP_TCP_SYNSCAN_PACKET_LEN;
 
 	return EXIT_SUCCESS;
 }
@@ -161,7 +164,7 @@ static fielddef_t fields[] = {
 
 probe_module_t module_ipv6_tcp_synscan = {
 	.name = "ipv6_tcp_synscan",
-	.max_packet_length = 74,
+	.max_packet_length = ZMAP_TCP_SYNSCAN_PACKET_LEN,
 	.pcap_filter = "ip6 proto 6 && (ip6[53] & 4 != 0 || ip6[53] == 18)",
 	.pcap_snaplen = 116, // was 96 for IPv4
 	.port_args = 1,
