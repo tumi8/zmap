83edf5ba9bc20289a8d0c198a9e94d6f04c9d1d4
Johannes Zirngibl <johannes.zirngibl@mpi-inf.mpg.de>
2024-12-16 10:23:48 +0100

    fix udp modules

--- src/probe_modules/module_ipv6_udp_dns.c
+++ src/probe_modules/module_ipv6_udp_dns.c
@@ -242,9 +242,8 @@ int ipv6_udp_dns_global_cleanup(__attribute__((unused)) struct state_conf *zconf
 	return EXIT_SUCCESS;
 }
 
-int ipv6_udp_dns_init_perthread(void* buf, macaddr_t *src,
-		macaddr_t *gw, __attribute__((unused)) port_h_t dst_port,
-        __attribute__((unused)) void **arg_ptr) {
+int ipv6_udp_dns_prepare_packet(void *buf, macaddr_t *src, macaddr_t *gw, UNUSED void *arg_ptr)
+{
 	memset(buf, 0, MAX_PACKET_SIZE);
 	struct ether_header *eth_header = (struct ether_header *) buf;
 	make_eth_header_ethertype(eth_header, src, gw, ETHERTYPE_IPV6);
@@ -418,8 +417,8 @@ probe_module_t module_ipv6_udp_dns = {
 	.pcap_filter = "ip6 proto 17 || icmp6",
 	.pcap_snaplen = 1500,			// TO BE CHANGED FOR EXSTIMATE REFLECTION SIZE
 	.port_args = 1,
-	.thread_initialize = &ipv6_udp_dns_init_perthread,
 	.global_initialize = &ipv6_udp_dns_global_initialize,
+	.prepare_packet = &ipv6_udp_dns_prepare_packet,
 	.make_packet = &ipv6_udp_dns_make_packet,
 	.print_packet = &ipv6_udp_dns_print_packet,
 	.validate_packet = &ipv6_udp_dns_validate_packet,
