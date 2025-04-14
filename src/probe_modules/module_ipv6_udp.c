d456d54e311569718e4d9812b00cefbe333c59aa
samuel.johnston <samuel.johnston@bitsighttech.com>
2025-04-14 11:34:41 -0400

    Enables templates for probing in ipv6.
    revert spacing changes

--- src/probe_modules/module_ipv6_udp.c
+++ src/probe_modules/module_ipv6_udp.c
@@ -292,6 +292,7 @@ int ipv6_udp_make_packet(void *buf, size_t *buf_len, struct in6_addr src_ip,
 		// The buf is a stack var of our caller of size MAX_PACKET_SIZE
 		// Recalculate the payload using the loaded template
 		payload_len = ipv6_udp_template_build(udp_template, payload, MAX_UDP_PAYLOAD_LEN, ip6_header, udp_header, aes);
+
 		// If success is zero, the template output was truncated
 		if (payload_len <= 0) {
 			log_fatal("udp", "UDP payload template generated an empty payload");
@@ -305,7 +306,7 @@ int ipv6_udp_make_packet(void *buf, size_t *buf_len, struct in6_addr src_ip,
 
 	udp_header->uh_sum = 0;
 	udp_header->uh_sum = ipv6_payload_checksum(ntohs(udp_header->uh_ulen), &ip6_header->ip6_src, &ip6_header->ip6_dst, (unsigned short *) udp_header, IPPROTO_UDP);
-
+	
 	size_t headers_len = sizeof(struct ether_header) + sizeof(struct ip6_hdr) +
 			     sizeof(struct udphdr);
 	*buf_len = headers_len + udp_send_msg_len;
